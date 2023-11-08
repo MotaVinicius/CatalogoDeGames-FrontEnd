2ec6bca18fc171508504f0a679b9014be4c73e78
Vinicius Mota <motavinicius@outlook.com>
2023-11-08 18:50:49 -0300

    search bar

--- CatalogoDeGames/src/App.jsx
+++ CatalogoDeGames/src/App.jsx
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
-import './App.css'
+
 import { Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Routes, Route } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Listagem from './components/listagem';
+import './App.css';
 
 function App() {
   return (
@@ -22,6 +25,15 @@ function App() {
                 <Nav.Link>Catalogo</Nav.Link>
             </LinkContainer>            
           </Nav>
+          <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button id='botao' variant="outline-success"><i class="fa-solid fa-magnifying-glass"></i></Button>
+                  </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- CatalogoDeGames/src/components/listagem.jsx
+++ CatalogoDeGames/src/components/listagem.jsx
@@ -14,13 +14,13 @@ export default function Listagem(){
         <div className="game-card">
                     {lista.map(itemLista => {
                         return(
-                            <Card style={{ width: '18rem', height:'30rem', backgroundColor: '#a8a8a8aa', margin:'2em' }}>
+                            <Card style={{ width: '18rem', height:'30rem', backgroundColor: '#a8a8a851', margin:'2em', color:"white" }}>
                                 <Card.Img style={{height:'20rem' }} variant="top" src={itemLista.image_url} alt={itemLista.name} key={itemLista._id} />
                                 <Card.Body>
                                     <div style={{display: 'flex', justifyContent:'space-evenly', flexDirection:'column'}}>
                                         <Card.Title style={{}}>{itemLista.name}</Card.Title>
                                         <Card.Subtitle style={{}}>{itemLista.genero}</Card.Subtitle>
-                                        <Button style={{marginTop:'1em'}} variant="danger">Detalhes</Button>
+                                        <Button style={{marginTop:'1.5em'}} variant="danger">Detalhes</Button>
                                     </div>
                                 </Card.Body>
                             </Card>
