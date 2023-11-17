7bcaef151a8088a3117c115fc6487cac668b7a60
Vinicius Mota <motavinicius@outlook.com>
2023-11-17 13:59:49 -0300

    LoginPage att

--- CatalogoDeGames/src/components/login.jsx
+++ CatalogoDeGames/src/components/login.jsx
@@ -1,5 +1,6 @@
 import { Form } from 'react-bootstrap';
 import '../login.css';
+import imagem from "./../image/game.svg"
 
 
 export default function Login(){
@@ -8,25 +9,31 @@ export default function Login(){
             <div className="topBar">
                 <h1><span>G</span>ame<span>Z</span>ada</h1>
             </div>
-
             <div id='body'>
-            <div id="content">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae tenetur, rerum, voluptatibus totam nisi ea non eum animi alias tempora quaerat eveniet, facilis eius similique incidunt sit voluptates magnam dignissimos!
-            </div>
-            <div className="loginForm">
-                <Form className="formulario">
-                <div id="loginHeader">
-                    <h2>Login</h2>
+                <div id="content">
+                <h2>Descubra seu próximo check-point</h2>
+               <p>Explore uma vasta seleção de jogos, desde épicos consagrados até as últimas novidades, descubra os jogos que estão em alta na comunidade ou busque informações sobre um game desejado.</p>
+                <p><span>Adicione os games mais cobissados por você a sua lista de desejos.</span></p>
+                <div id="img">
+                <img src={imagem} alt="Game" /></div>
                 </div>
-                    
+                <div className="loginForm">
+                    <Form className="formulario">
+                    <div id="loginHeader">
+                        <h2>Login</h2>
+                    </div>
                     <div id="fields">
                         <div id="usuario">
                             <label>Usuario:</label>
-                            <input type="text" placeholder='Nome de usuario'/>
+                            <div id="fieldUser">
+                            <i class="fa-solid fa-user"></i><input type="text" placeholder='Nome de usuario'/>
+                            </div>
                         </div>
                         <div id="senha">
                             <label>Senha:</label>
-                            <input type="password" placeholder='Digite sua senha'/>
+                            <div id="fieldSenha">
+                            <i class="fa-solid fa-key"></i><input type="password" placeholder='Digite sua senha'/>
+                            </div>
                         </div>
                     </div>
                     <button>Entrar</button>         
